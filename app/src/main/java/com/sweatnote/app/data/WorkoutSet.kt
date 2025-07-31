49e4bc651960adb405d34fc8ec199741353f644e
Sachin Pandey <33sachinedu@gmail.com>
2025-07-31 20:36:35 +0530

    Create WorkoutSet

---
+++ app/src/main/java/com/sweatnote/app/data/WorkoutSet.kt
@@ -0,0 +1,12 @@
+package com.sweatnote.app.data
+
+import java.util.UUID
+
+data class WorkoutSet(
+    val id: UUID = UUID.randomUUID(),
+    var weight: String = "",
+    var reps: String = "",
+    var isCompleted: Boolean = false
+)
+
+data class LiveWorkoutState(val exercise: Exercise, val sets: MutableList<WorkoutSet> = mutableListOf(WorkoutSet()))
