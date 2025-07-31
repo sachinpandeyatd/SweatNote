c75af592422449511a46edb263b26761221b76b0
Sachin Pandey <33sachinedu@gmail.com>
2025-07-31 12:39:22 +0530

    Create Nav host

---
+++ app/src/main/java/com/sweatnote/app/navigation/AppNavHost.kt
@@ -0,0 +1,29 @@
+package com.sweatnote.app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.sweatnote.app.ui.screens.DashboardScreen
+import com.sweatnote.app.ui.screens.HistoryScreen
+import com.sweatnote.app.ui.screens.ProfileScreen
+import com.sweatnote.app.ui.screens.RoutinesScreen
+
+@Composable
+fun AppNavHost(navController: NavHostController, modifier: Modifier = Modifier){
+    NavHost(navController = navController, startDestination = Screen.Dashboard.route, modifier = modifier){
+        composable(Screen.Dashboard.route){
+            DashboardScreen()
+        }
+        composable(Screen.History.route){
+            HistoryScreen()
+        }
+        composable(Screen.Routines.route){
+            RoutinesScreen()
+        }
+        composable(Screen.Profile.route){
+            ProfileScreen()
+        }
+    }
+}
